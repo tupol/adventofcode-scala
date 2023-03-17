762ac00d9a314c2e98b8a02d68c1bb141b6070d5
Oliver Tupran <oliver.tupran@schiphol.nl>
2023-03-17 18:24:52 +0100

    day 05 - solved

---
+++ src/main/scala/day05/package.scala
@@ -0,0 +1,60 @@
+import scala.collection.mutable.Stack
+
+package object day05 {
+
+  class Storage[T](private val stacks: Array[Stack[T]]) {
+
+    require(stacks.size > 0, "The storage needs to have at least a stack slot")
+
+    def show =
+      stacks.zipWithIndex.foreach{ case (stack, idx) => println(s"[${idx+1}] ${stack.reverse.mkString(" ")}")}
+
+    private def checkSlot(slot: Int) =
+      require(slot >= 0 && slot < stacks.size)
+
+    def fill(slot: Int, elements: Seq[T]): Unit =
+      checkSlot(slot)
+      elements.foreach(x => add(slot, x))
+
+
+    def add(slot: Int, element: T): Unit =
+      checkSlot(slot)
+      stacks(slot).push(element)
+
+    def move(command: MoveCommand): Unit =
+      move(command.from-1, command.to-1, command.amount)
+
+    def move(from: Int, to: Int, amount: Int): Unit =
+      checkSlot(from)
+      checkSlot(to)
+      (0 until amount).foreach(_ => add(to, stacks(from).pop()))
+
+    def moveBulk(command: MoveCommand): Unit =
+      moveBulk(command.from - 1, command.to - 1, command.amount)
+    def moveBulk(from: Int, to: Int, amount: Int): Unit =
+      checkSlot(from)
+      checkSlot(to)
+      val tempStack = Stack.empty[T]
+      (0 until amount).foreach{ _ => tempStack.push(stacks(from).pop()) }
+      fill(to, tempStack.toSeq)
+
+    def topContainers(): Seq[T] = stacks.toSeq.map(_.top)
+
+  }
+
+  object Storage {
+    def apply[T](stacks: Int) = new Storage(Array.fill(stacks)(Stack.empty[T]))
+  }
+
+  case class MoveCommand(from: Int, to: Int, amount: Int) {
+    override def toString: String = s"move $amount from $from to $to"
+  }
+
+  object MoveCommand {
+    private val pattern = "move (\\d+) from (\\d+) to (\\d+)".r
+    def apply(input: String): MoveCommand =
+      input match
+        case pattern(amount, from, to) if amount.toInt >= 0 => MoveCommand(from.toInt, to.toInt, amount.toInt)
+        case str => throw new IllegalArgumentException(s"'$str' is an invalid command string")
+  }
+}
---
+++ src/main/scala/day05/part2.scala
@@ -0,0 +1,61 @@
+package day05
+
+import scala.collection.immutable.Seq
+import scala.io.*
+import scala.util.{Failure, Try}
+
+@main
+def part2(): Unit = {
+
+  val testStorage = Storage[String](3)
+  testStorage.fill(0, Seq("Z", "N"))
+  testStorage.fill(1, Seq("M", "C", "D"))
+  testStorage.fill(2, Seq("P"))
+  testStorage.show
+
+  val commands = Seq(
+    "move 1 from 2 to 1",
+    "move 3 from 1 to 3",
+    "move 2 from 2 to 1",
+    "move 1 from 1 to 2"
+  ).map(MoveCommand(_))
+
+  commands.foreach{ testStorage.moveBulk }
+
+  testStorage.show
+
+  println(testStorage.topContainers().mkString(" "))
+
+  //    [H]         [H]         [V]
+  //    [V]         [V] [J]     [F] [F]
+  //    [S] [L]     [M] [B]     [L] [J]
+  //    [C] [N] [B] [W] [D]     [D] [M]
+  //[G] [L] [M] [S] [S] [C]     [T] [V]
+  //[P] [B] [B] [P] [Q] [S] [L] [H] [B]
+  //[N] [J] [D] [V] [C] [Q] [Q] [M] [P]
+  //[R] [T] [T] [R] [G] [W] [F] [W] [L]
+  // 1   2   3   4   5   6   7   8   9
+
+  val mainStorage = Storage[Char](9)
+  mainStorage.fill(0, "RNPG".toCharArray.toSeq)
+  mainStorage.fill(1, "TJBLCSVH".toCharArray.toSeq)
+  mainStorage.fill(2, "TDBMNL".toCharArray.toSeq)
+  mainStorage.fill(3, "RVPSB".toCharArray.toSeq)
+  mainStorage.fill(4, "GCQSWMVH".toCharArray.toSeq)
+  mainStorage.fill(5, "WQSCDBJ".toCharArray.toSeq)
+  mainStorage.fill(6, "FQL".toCharArray.toSeq)
+  mainStorage.fill(7, "WMHTDLFV".toCharArray.toSeq)
+  mainStorage.fill(8, "LPBVMJF".toCharArray.toSeq)
+  mainStorage.show
+
+  val mainCommands = Source.fromResource("day05/input.txt").getLines()
+    .filter(_.startsWith("move"))
+    .map(MoveCommand(_))
+
+  mainCommands.foreach(mainStorage.moveBulk)
+
+  mainStorage.show
+
+  println(mainStorage.topContainers().mkString(""))
+
+}
