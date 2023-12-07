438fffe32f0b573d5372234dfe7d0e6f5f067fba
Oliver Tupran <oliver.tupran@schiphol.nl>
2023-12-07 17:27:35 +0100

    AOC 2023 Day 7 - Nicer

--- src/main/scala/aoc2023/day07/solution.scala
+++ src/main/scala/aoc2023/day07/solution.scala
@@ -3,6 +3,7 @@ package aoc2023.day07
 
 import aoc2023.day07.solution.Card.*
 import aoc2023.day07.solution.HandType.*
+import aoc2023.day07.solution.Part1Rules
 
 import scala.io.Source
 import scala.math
@@ -10,77 +11,45 @@ import scala.util.*
 
 object solution extends App {
 
-  sealed trait Card {
-    def value1: Int
-
-    def value2: Int = value1
-  }
+  sealed trait Card
   object Card {
-
     case object C2 extends Card {
-      val value1 = 2;
       override def toString: String = "2";
     }
-
     case object C3 extends Card {
-      val value1 = 3
       override def toString: String = "3";
     }
-
     case object C4 extends Card {
-      val value1 = 4;
       override def toString: String = "4";
     }
-
     case object C5 extends Card {
-      val value1 = 5;
       override def toString: String = "5";
     }
-
     case object C6 extends Card {
-      val value1 = 6;
       override def toString: String = "6";
     }
-
     case object C7 extends Card {
-      val value1 = 7;
       override def toString: String = "7";
     }
-
     case object C8 extends Card {
-      val value1 = 8;
       override def toString: String = "8";
     }
-
     case object C9 extends Card {
-      val value1 = 9;
       override def toString: String = "9";
     }
-
     case object CT extends Card {
-      val value1 = 10;
       override def toString: String = "T";
     }
-
     case object CJ extends Card {
-      val value1 = 11;
-
-      override val value2 = 1;
       override def toString: String = "J";
     }
-
     case object CQ extends Card {
-      val value1 = 12;
       override def toString: String = "Q";
     }
-
     case object CK extends Card {
-      val value1 = 13;
       override def toString: String = "K";
     }
-
     case object CA extends Card {
-      val value1 = 14;
       override def toString: String = "A";
     }
 
@@ -104,36 +73,28 @@ object solution extends App {
 
 
   sealed trait HandType {
-    def value: Int
     def bumpHand: HandType
   }
   object HandType {
     case object HighCard extends HandType {
-      val value = 0
       def bumpHand = OnePair
     }
     case object OnePair extends HandType {
-      val value = 1
       def bumpHand = ThreeOfAKind
     }
     case object TwoPair extends HandType {
-      val value = 2
       def bumpHand = FullHouse
     }
     case object ThreeOfAKind extends HandType {
-      val value = 3
       def bumpHand = FourOfAKind
     }
     case object FullHouse extends HandType {
-      val value = 4
       def bumpHand = FourOfAKind
     }
     case object FourOfAKind extends HandType {
-      val value = 5
       def bumpHand = FiveOfAKind
     }
     case object FiveOfAKind extends HandType {
-      val value = 6
       def bumpHand = FiveOfAKind
     }
   }
@@ -141,27 +102,7 @@ object solution extends App {
 
   case class Hand(c1: Card, c2: Card, c3: Card, c4: Card, c5: Card) {
     val cards = Seq(c1, c2, c3, c4, c5)
-    lazy val handType1: HandType = {
-      val cardGroups = cards.groupBy(identity).map { case (c, cx) => (c, cx.size) }.toSeq.sortBy(_._2).reverse
-      cardGroups match {
-        case (card, 5) +: Nil => FiveOfAKind
-        case (card1, 4) +: (card2, count2) +: Nil => FourOfAKind
-        case (card1, 3) +: (card2, 2) +: Nil => FullHouse
-        case (card1, 3) +: _ => ThreeOfAKind
-        case (card1, 2) +: (card2, 2) +: _ => TwoPair
-        case (card1, 2) +: _ => OnePair
-        case _ => HighCard
-      }
-    }
-    lazy val handType2 = {
-      val initialRank = handType1
-      if(handType1 == OnePair && cards.filter(_ == CJ).size == 2)
-        ThreeOfAKind
-      else
-        cards.filter(_ == CJ).foldLeft(initialRank)((r, _) => r.bumpHand)
-    }
     override def toString: String = cards.mkString("")
-    def toGrouppedString: String = cards.sortBy(_.value1).reverse.mkString("")
   }
   object Hand{
     def fromString(input: String): Hand = {
@@ -169,35 +110,6 @@ object solution extends App {
       parsedCards match {
         case Seq(c1, c2, c3, c4, c5) => Hand(c1, c2, c3, c4, c5)
         case cx => throw new IllegalAccessException(s"A hand can have 5 and only 5 cards, but we got ${cx.size} in ${cx.mkString("")}")
-      }
-    }
-  }
-
-  val Hand1Ordering: Ordering[Hand] = new Ordering[Hand] {
-    override def compare(x: Hand, y: Hand): Int = {
-      if (x.handType1.value < y.handType1.value)
-        -1
-      else if (x.handType1.value > y.handType1.value)
-        1
-      else {
-        x.cards.zip(y.cards).foldLeft(0) { case (ord, (xc, yc)) =>
-          if (ord == 0) Ordering[Int].compare(xc.value1, yc.value1)
-          else ord
-        }
-      }
-    }
-  }
-  val Hand2Ordering: Ordering[Hand] = new Ordering[Hand] {
-    override def compare(x: Hand, y: Hand): Int = {
-      if (x.handType2.value < y.handType2.value)
-        -1
-      else if (x.handType2.value > y.handType2.value)
-        1
-      else {
-        x.cards.zip(y.cards).foldLeft(0) { case (ord, (xc, yc)) =>
-          if (ord == 0) Ordering[Int].compare(xc.value2, yc.value2)
-          else ord
-        }
       }
     }
   }
@@ -210,21 +122,83 @@ object solution extends App {
         case _ => throw new IllegalAccessException(s"The input does not look as a playing hand: $input")
   }
 
-  def winnings1(playHands: Seq[PlayHand]): Long = {
-    val rankedHands = playHands.sortBy(_.hand)(Hand1Ordering).zipWithIndex.map { case (ph, ix) => (ph, (ix + 1)) }
-//    rankedHands.map(rh => f"${rh._1.hand}  ${rh._1.bet}%4d$$  ${rh._2}%4d  |  ${rh._1.hand.toGrouppedString} ${rh._1.hand.handType1}").foreach(println)
-//    rankedHands.map(rh => f"${rh._1.bet},${rh._2}").foreach(println)
-    val amount = rankedHands.map { case (ph, rank) => ph.bet * rank }.sum
-    amount
+  def handOrdering(cardValue: Card => Int, handTypeValue: Hand => Int) = new Ordering[Hand] {
+    override def compare(x: Hand, y: Hand): Int = {
+      if (handTypeValue(x) < handTypeValue(y))
+        -1
+      else if (handTypeValue(x) > handTypeValue(y))
+        1
+      else {
+        x.cards.zip(y.cards).foldLeft(0) { case (ord, (xc, yc)) =>
+          if (ord == 0) Ordering[Int].compare(cardValue(xc), cardValue(yc))
+          else ord
+        }
+      }
+    }
   }
 
-  def winnings2(playHands: Seq[PlayHand]): Long = {
-    val rankedHands = playHands.sortBy(_.hand)(Hand2Ordering).zipWithIndex.map { case (ph, ix) => (ph, (ix + 1)) }
-    //    rankedHands.map(rh => f"${rh._1.hand}  ${rh._1.bet}%4d$$  ${rh._2}%4d  |  ${rh._1.hand.toGrouppedString} ${rh._1.hand.handType2}").foreach(println)
-    //    rankedHands.map(rh => f"${rh._1.bet},${rh._2}").foreach(println)
-    val amount = rankedHands.map { case (ph, rank) => ph.bet * rank }.sum
-    amount
+  object Part1Rules {
+    private def cardsOrderMap: Map[Card, Int] = Seq(C2, C3, C4, C5, C6, C7, C8, C9, CT, CJ, CQ, CK, CA).zipWithIndex.toMap
+    private def handsOrderMap: Map[HandType, Int] = Seq(HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind).zipWithIndex.toMap
+
+    def cardValue(card: Card) = cardsOrderMap(card)
+    def handTypeValue(hand: Hand): Int = handsOrderMap(handType(hand))
+
+    implicit val HandOrdering: Ordering[Hand] = handOrdering(cardValue, handTypeValue)
+
+    def prettyHand(hand: Hand): String = hand.cards.sortBy(cardValue).reverse.mkString("")
+
+    def handType(hand: Hand): HandType = {
+      val cardGroups = hand.cards.groupBy(identity).map { case (c, cx) => (c, cx.size) }.toSeq.sortBy(_._2).reverse
+      cardGroups match {
+        case (card, 5) +: Nil => FiveOfAKind
+        case (card1, 4) +: (card2, count2) +: Nil => FourOfAKind
+        case (card1, 3) +: (card2, 2) +: Nil => FullHouse
+        case (card1, 3) +: _ => ThreeOfAKind
+        case (card1, 2) +: (card2, 2) +: _ => TwoPair
+        case (card1, 2) +: _ => OnePair
+        case _ => HighCard
+      }
+    }
+    def winnings(playHands: Seq[PlayHand]): Long = {
+      val rankedHands = playHands.sortBy(_.hand).zipWithIndex.map { case (ph, ix) => (ph, (ix + 1)) }
+//      rankedHands.map(rh => f"${rh._1.hand}  ${rh._1.bet}%4d$$  ${rh._2}%4d  |  ${prettyHand(rh._1.hand)} ${handType(rh._1.hand)}").foreach(println)
+//          rankedHands.map(rh => f"${rh._1.bet},${rh._2}").foreach(println)
+      val amount = rankedHands.map { case (ph, rank) => ph.bet * rank }.sum
+      amount
+    }
   }
+
+  object Part2Rules {
+    private def cardsOrderMap: Map[Card, Int] = Seq(CJ, C2, C3, C4, C5, C6, C7, C8, C9, CT, CQ, CK, CA).zipWithIndex.toMap
+
+    private def handsOrderMap: Map[HandType, Int] = Seq(HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind).zipWithIndex.toMap
+
+    def cardValue(card: Card) = cardsOrderMap(card)
+
+    def handTypeValue(hand: Hand): Int = handsOrderMap(handType(hand))
+
+    implicit val HandOrdering: Ordering[Hand] = handOrdering(cardValue, handTypeValue)
+
+    def prettyHand(hand: Hand): String = hand.cards.sortBy(cardValue).reverse.mkString("")
+
+    def handType(hand: Hand): HandType = {
+      val initialRank = Part1Rules.handType(hand)
+      if (initialRank == OnePair && hand.cards.filter(_ == CJ).size == 2)
+        ThreeOfAKind
+      else
+        hand.cards.filter(_ == CJ).foldLeft(initialRank)((r, _) => r.bumpHand)
+    }
+
+    def winnings(playHands: Seq[PlayHand]): Long = {
+      val rankedHands = playHands.sortBy(_.hand).zipWithIndex.map { case (ph, ix) => (ph, (ix + 1)) }
+//      rankedHands.map(rh => f"${rh._1.hand}  ${rh._1.bet}%4d$$  ${rh._2}%4d  |  ${prettyHand(rh._1.hand)} ${handType(rh._1.hand)}").foreach(println)
+//      rankedHands.map(rh => f"${rh._1.bet},${rh._2}").foreach(println)
+      val amount = rankedHands.map { case (ph, rank) => ph.bet * rank }.sum
+      amount
+    }
+  }
+
 
   val sampleCards =
     """
@@ -236,17 +210,16 @@ object solution extends App {
       |""".stripMargin.split("\n").map(_.trim).filterNot(_.isEmpty)
 
 
-  val sampleResult1 = winnings1(sampleCards.map(PlayHand.fromString))
+  val sampleResult1 = Part1Rules.winnings(sampleCards.map(PlayHand.fromString))
   println(s"Sample 1: $sampleResult1") // 288
 
   def input = Source.fromResource("aoc2023/day07/input.txt").getLines()
-  val result1 = winnings1(input.map(PlayHand.fromString).toSeq)
+  val result1 = Part1Rules.winnings(input.map(PlayHand.fromString).toSeq)
   println(s"Part 1: $result1") // 252656917
 
-
-  val sampleResult2 = winnings2(sampleCards.map(PlayHand.fromString))
+  val sampleResult2 = Part2Rules.winnings(sampleCards.map(PlayHand.fromString))
   println(s"Sample 2: $sampleResult2") // 5905
 
-  val result2 = winnings2(input.map(PlayHand.fromString).toSeq)
+  val result2 = Part2Rules.winnings(input.map(PlayHand.fromString).toSeq)
   println(s"Part 2: $result2") // 253499763
 }
